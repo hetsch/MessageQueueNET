363ae137f5ba7ab9872f3c1c5492e7ce51b264fb
jugstalt <gstalt@gmx.at>
2021-04-22 21:28:51 +0200

    commit

--- MessageQueueNET.Client/QueueClient.cs
+++ MessageQueueNET.Client/QueueClient.cs
@@ -5,7 +5,7 @@ using System.Text.Json;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MessageQueue.NET.Client
+namespace MessageQueueNET.Client
 {
     public class QueueClient
     {
--- MessageQueueNET.Cmd/Program.cs
+++ MessageQueueNET.Cmd/Program.cs
@@ -1,4 +1,4 @@
-﻿using MessageQueue.NET.Client;
+﻿using MessageQueueNET.Client;
 using System;
 using System.Collections.Generic;
 using System.IO;
