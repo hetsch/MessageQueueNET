a968d853b13f683bc8ac09173c5eb1124edd850d
Jürgen Gstaltmaier <gstalt@live.com>
2022-01-20 14:51:58 +0100

    sync

---
+++ MessageQueueNET.ProcOrchestra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageQueueNET.ProcOrchestra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
