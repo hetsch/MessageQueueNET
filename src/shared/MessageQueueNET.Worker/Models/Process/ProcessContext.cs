9e77713e873307afd6d235de757680c59abad58b
jugstalt <gstalt@gmx.at>
2023-11-02 11:50:35 +0100

    commit

--- src/shared/MessageQueueNET.Worker/Models/Process/ProcessContext.cs
+++ src/shared/MessageQueueNET.Worker/Models/Process/ProcessContext.cs
@@ -10,14 +10,4 @@ public class ProcessContext
     public int ExitCode { get; set; }
     public string? Output { get; set; }
     public string? ErrorOutput { get; set; }
-
-    #region ITaskContext
-
-    public long TaskId { get; set; }
-
-    public DateTime StartTime { get; set; }
-
-    public string LogFile { get; set; } = string.Empty;
-
-    #endregion
 }
