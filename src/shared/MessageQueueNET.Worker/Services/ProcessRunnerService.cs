0916774afd7446ed71c33d12ca89ad2a5c26eb95
jugstalt <gstalt@gmx.at>
2023-11-07 15:29:52 +0100

    logging

--- src/shared/MessageQueueNET.Worker/Services/ProcessRunnerService.cs
+++ src/shared/MessageQueueNET.Worker/Services/ProcessRunnerService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MessageQueueNET.Worker.Services;
 
-public class ProcessRunnerService
+public partial class ProcessRunnerService
 {
     private readonly ILogger<ProcessRunnerService> _logger;
 
@@ -20,62 +20,69 @@ public class ProcessRunnerService
     {
         var task = new Task(() =>
         {
-            var processStartInfo = new ProcessStartInfo();
-
-            processStartInfo.FileName = context.Command;
-            processStartInfo.Arguments = context.Arguments;
-
-            processStartInfo.CreateNoWindow = true;
-            processStartInfo.UseShellExecute = false;
-            processStartInfo.RedirectStandardOutput = true;
-            processStartInfo.RedirectStandardError = true;
-
-            _logger.LogInformation("Starting process {command} with arguments: {arguments}",
-                context.Command, context.Arguments);
-
-            using (Process process = Process.Start(processStartInfo)!)
+            try
             {
-                StringBuilder output = new(),
-                              stdErr = new();
+                var processStartInfo = new ProcessStartInfo();
 
-                process.OutputDataReceived += (sender, outLine) =>
+                processStartInfo.FileName = context.Command;
+                processStartInfo.Arguments = context.Arguments;
+
+                processStartInfo.CreateNoWindow = true;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.RedirectStandardError = true;
+
+                LogStartingProcess(context.Command, context.Arguments);
+
+                using (Process process = Process.Start(processStartInfo)!)
                 {
-                    if (!String.IsNullOrEmpty(outLine.Data))
+                    StringBuilder output = new(),
+                                  stdErr = new();
+
+                    process.OutputDataReceived += (sender, outLine) =>
                     {
-                        //Console.WriteLine($"Output received:{ outLine.Data }");
-                        output.AppendLine(outLine.Data);
-                    }
-                };
+                        if (!String.IsNullOrEmpty(outLine.Data))
+                        {
+                            //Console.WriteLine($"Output received:{ outLine.Data }");
+                            output.AppendLine(outLine.Data);
+                        }
+                    };
 
-                process.ErrorDataReceived += (sender, outLine) =>
-                {
-                    if (!String.IsNullOrEmpty(outLine.Data))
+                    process.ErrorDataReceived += (sender, outLine) =>
                     {
-                        //Console.WriteLine($"Error received:{ outLine.Data }");
-                        output.AppendLine(outLine.Data);
-                        stdErr.AppendLine(outLine.Data);
-                    }
-                };
+                        if (!String.IsNullOrEmpty(outLine.Data))
+                        {
+                            //Console.WriteLine($"Error received:{ outLine.Data }");
+                            output.AppendLine(outLine.Data);
+                            stdErr.AppendLine(outLine.Data);
+                        }
+                    };
 
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
 
-                process.WaitForExit();
+                    process.WaitForExit();
 
-                // Causes Deadlogs...
-                //while (process.StandardOutput.Peek() > -1)
-                //{
-                //    output.Add(process.StandardOutput.ReadLine());
-                //}
+                    // Causes Deadlogs...
+                    //while (process.StandardOutput.Peek() > -1)
+                    //{
+                    //    output.Add(process.StandardOutput.ReadLine());
+                    //}
 
-                //while (process.StandardError.Peek() > -1)
-                //{
-                //    output.Add(process.StandardError.ReadLine());
-                //}
+                    //while (process.StandardError.Peek() > -1)
+                    //{
+                    //    output.Add(process.StandardError.ReadLine());
+                    //}
 
-                context.ExitCode = process.ExitCode;
-                context.Output = output.ToString();
-                context.ErrorOutput = stdErr.Length > 0 ? stdErr.ToString() : null;
+                    context.ExitCode = process.ExitCode;
+                    context.Output = output.ToString();
+                    context.ErrorOutput = stdErr.Length > 0 ? stdErr.ToString() : null;
+                }
+            } 
+            catch(Exception ex)
+            {
+                LogExceptionMessage(context.Command, ex.Message);
+                throw;
             }
         });
 
@@ -83,4 +90,16 @@ public class ProcessRunnerService
 
         return task;
     }
+
+    [LoggerMessage(
+        EventId = 0,
+        Level = LogLevel.Information,
+        Message = "Starting process {command} with arguments: {arguments}")]
+    public partial void LogStartingProcess(string command, string arguments);
+
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Error,
+        Message = "Error processing command {command}: {exceptionMessage}")]
+    public partial void LogExceptionMessage(string command, string exceptionMessage);
 }
